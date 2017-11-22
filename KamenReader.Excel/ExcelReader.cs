7629d3c608937ab654af80bc2bfb7109dadf891a
asakura89 <flamboyan3no1a@gmail.com>
2017-11-22 21:06:06 +0700

    not used maps error fix

--- KamenReader.Excel/ExcelReader.cs
+++ KamenReader.Excel/ExcelReader.cs
@@ -1,7 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
-using System.Linq;
 using SpreadsheetLight;
 
 namespace KamenReader.Excel
@@ -10,27 +9,21 @@ namespace KamenReader.Excel
     {
         public FileReaderResult Read(String fullFilepath, IList<FileReaderMap> maps, Boolean firstRowAreTitles = true)
         {
-            return Read(fullFilepath, maps, "", firstRowAreTitles);
-        }
-
-        public FileReaderResult Read(String fullFilepath, IList<FileReaderMap> maps, String sheetName = "", Boolean firstRowAreTitles = true)
-        {
             if (String.IsNullOrEmpty(fullFilepath))
                 throw new ArgumentException("fullFilepath");
             if (!File.Exists(fullFilepath))
                 throw new InvalidOperationException("File's not found.");
-            if (maps == null)
-                throw new ArgumentException("maps");
-            if (!maps.Any())
-                throw new InvalidOperationException("File Reader Map must be supplied.");
 
             var result = new FileReaderResult();
             using (var doc = new SLDocument(fullFilepath))
             {
+                /*
                 if (String.IsNullOrEmpty(sheetName))
                     sheetName = doc.GetCurrentWorksheetName();
 
                 doc.SelectWorksheet(sheetName);
+                */
+                doc.SelectWorksheet(doc.GetCurrentWorksheetName());
                 var stats = doc.GetWorksheetStatistics();
                 for (int row = 1; row <= stats.EndRowIndex; row++)
                 {
--- KamenReader.Excel/Properties/AssemblyVersion.cs
+++ KamenReader.Excel/Properties/AssemblyVersion.cs
@@ -1,4 +1,4 @@
 using System.Reflection;
 
-[assembly: AssemblyVersion("1.0.2017.0119")]
-[assembly: AssemblyFileVersion("1.0.2017.0119")]
+[assembly: AssemblyVersion("1.0.2017.0122")]
+[assembly: AssemblyFileVersion("1.0.2017.0122")]
